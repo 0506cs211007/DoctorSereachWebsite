827a9baf80852d833d1e5480de92742254dab73b
AkshatrajSinghTomar <157144754+0506cs211007@users.noreply.github.com>
2024-05-31 22:31:06 -0700

    Update app.js

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/DoctorSearch", async (req, resp) => {
+app.get("/", async (req, resp) => {
   const data = await Listings.find({});
   resp.render("Home.ejs",{ data });
 });
